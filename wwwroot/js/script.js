43d22b5bc7115b52e34662b2238c733ab31cac10
connorenders <144719211+connorenders@users.noreply.github.com>
2023-10-06 11:46:29 -0500

    Add files via upload

---
+++ wwwroot/js/script.js
@@ -0,0 +1,35 @@
+function apiSearch() {
+    var params = {
+        'q': $('#query').val(),
+        'count': 50,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+
+    $.ajax({
+        url: 'https://https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '79a2f58b34d4411c940cb6d22d5074a5'
+        }
+    })
+        .done(function (data) {
+            var len = data.webPages.value.length;
+            var results = '';
+            for (i = 0; i < len; i++) {
+                results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
+            }
+
+            $('#searchResults').html(results);
+            $('#searchResults').dialog();
+        })
+        .fail(function () {
+            alert('error');
+        });
+
+
+    function Hello() {
+        alert("Hello, World");
+    }
+
+}
